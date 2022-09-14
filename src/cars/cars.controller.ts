c761658da49e68336e55db9ece29ca9256e8e217
ggonzalezh <ggonzalezh15@gmail.com>
2022-09-14 17:08:36 -0300

    Section 3

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,24 @@
+import {Body, Controller, Get, Param, ParseIntPipe, Post} from '@nestjs/common';
+import {CarsService} from "./cars.service";
+
+@Controller('cars')
+export class CarsController {
+
+    constructor(private readonly carsService: CarsService) {}
+
+    @Get()
+    getAllCars() {
+        return this.carsService.findAll()
+    }
+
+    @Get(':id')
+    getCarsById(@Param('id', ParseIntPipe) id : number) {
+        return this.carsService.findOneById(id)
+    }
+
+    @Post()
+    createCar(@Body() body: any) {
+        return body
+    }
+
+}
